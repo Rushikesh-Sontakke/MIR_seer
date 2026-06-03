719b20bea354aa02c9a1ab2ca459d8bc3c2f4ada
Rushikesh-Sontakke <rushikesh@gapp.nthu.edu.tw>
2026-06-03 23:03:56 +0800

    Update batch size to 500 in training script; add model info JSON file with best validation loss

--- training/train.py
+++ training/train.py
@@ -44,7 +44,7 @@ MODEL_INFO_PATH = os.path.join(ROOT_DIR, "seer_model_info.json")
 # ==============================================================
 
 SEQUENCE_LENGTH = 500       # median MIDI length from the paper
-BATCH_SIZE = 256              # 500 in the paper
+BATCH_SIZE = 500              # 500 in the paper
 LEARNING_RATE = 1e-3
 EPOCHS = 15
 TEST_RATIO = 0.2
